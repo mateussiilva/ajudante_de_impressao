67c1aac4aff726c5ed96753693c59e5fb0f5c0f5
mateussiilva <mateuspython3010@gmail.com>
2023-12-12 20:58:51 -0300

    Terminando as funções de calculo de tempo de impressão

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 #!/usr/bin/env nodejs
 
 
+const configuracoesImpressoras = {
+    "1604": 9,
+    "1602": 17,
+    "1904": 58,
+};
+const MINUTOS = 60;
+
+function calcularTempoImpressao(metros_por_minuto, metros) {
+    return Math.ceil(metros / metros_por_minuto * 60)
+}
 
 
-document.addEventListener("submit", event => {
-    event.preventDefault()
-})
+function TempoDeImpressão() {
+    let metrosImpressao = parseFloat(document.querySelector("#imetros").value);
+    let impressoraSelecionada = document.querySelector("#iimpressoras").value;
+    for (impresora in configuracoesImpressoras) {
+        if (impresora === impressoraSelecionada) {
+            let metrosPorMinuto = configuracoesImpressoras[impresora]
+            alert(`${calcularTempoImpressao(metrosPorMinuto, metrosImpressao)} minutos`)
+        }
+    }
+
+}
